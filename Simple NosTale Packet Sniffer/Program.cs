5540552d84ea08bf9353b9a839f180f5ff394a22
Zanou <noszanou@gmail.com>
2023-06-30 18:06:51 +0200

    Issou.

--- Simple NosTale Packet Sniffer/Program.cs
+++ Simple NosTale Packet Sniffer/Program.cs
@@ -20,6 +20,7 @@ namespace Simple_NosTale_Packet_Sniffer
                 return;
             }
 
+            // Choose your interface there :issou:
             var device = devices.First();
             device.OnPacketArrival += Device_OnPacketArrival;
             device.Open(DeviceModes.Promiscuous, 1000);
@@ -64,13 +65,13 @@ namespace Simple_NosTale_Packet_Sniffer
                 return;
             }
 
-            if (srcIp.ToString().Equals(StaticConfig.GameForgeAsgobasServerIp))
+            if (srcIp.ToString().Equals(StaticConfig.GameForgeValehirServerIp))
             {
                 ParseWorldReceivePacket(data);
                 return;
             }
 
-            if (dstIp.ToString().Equals(StaticConfig.GameForgeAsgobasServerIp))
+            if (dstIp.ToString().Equals(StaticConfig.GameForgeValehirServerIp))
             {
                 ParseWorldSendPacket(data);
                 return;
@@ -79,96 +80,112 @@ namespace Simple_NosTale_Packet_Sniffer
 
         private static void ParseLoginReceivePacket(byte[] data)
         {
-            Console.WriteLine("[LOGIN][RECV] {0}", LoginPacketExtension.ServerToClient(data));
+            try
+            {
+                Console.WriteLine("[LOGIN][RECV] {0}", LoginPacketExtension.ServerToClient(data));
+            }
+            catch { }
         }
 
         private static void ParseWorldReceivePacket(byte[] data)
         {
-            var pac = WorldPacketExtension.ServerToClient(data);
-            foreach (var i in pac)
+            try
             {
-                Console.WriteLine("[WORLD][RECV] {0}", i);
+                var pac = WorldPacketExtension.ServerToClient(data);
+                foreach (var i in pac)
+                {
+                    Console.WriteLine("[WORLD][RECV] {0}", i);
+                }
             }
+            catch { }
         }
 
         private static void ParseLoginSendPacket(byte[] data)
         {
-            Console.WriteLine("[LOGIN][SEND] {0}", LoginPacketExtension.ClientToServer(data));
+            try
+            {
+                Console.WriteLine("[LOGIN][SEND] {0}", LoginPacketExtension.ClientToServer(data));
+            }
+            catch { }
         }
 
         private static void ParseWorldSendPacket(byte[] data)
         {
-            if (SessionId == 0)
+            try
             {
-                string sessionPacket = WorldPacketExtension.InitializeEncryptionKey(data);
-
-                string[] sessionParts = sessionPacket.Split(' ');
-
-                if (sessionParts.Length == 0)
+                if (SessionId == 0)
                 {
+                    string sessionPacket = WorldPacketExtension.InitializeEncryptionKey(data);
+
+                    string[] sessionParts = sessionPacket.Split(' ');
+
+                    if (sessionParts.Length == 0)
+                    {
+                        return;
+                    }
+
+                    if (!int.TryParse(sessionParts[0], out int packetId))
+                    {
+                        return;
+                    }
+
+                    LastPacketId = packetId;
+                    if (sessionParts.Length < 2)
+                    {
+                        return;
+                    }
+
+                    if (int.TryParse(sessionParts[1].Split('\\').FirstOrDefault(), out int sessid))
+                    {
+                        SessionId = sessid;
+                    }
+
                     return;
                 }
 
-                if (!int.TryParse(sessionParts[0], out int packetId))
+                string packetConcatenated = WorldPacketExtension.ClientToServer(data, SessionId);
+
+                foreach (string pd in packetConcatenated.Split(new[] { (char)0xFF }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    return;
-                }
+                    string packetstring = pd.Replace('^', ' ');
+                    string[] packetsplit = packetstring.Split(' ');
+                    string nextRawPacketId = packetsplit[0];
 
-                LastPacketId = packetId;
-                if (sessionParts.Length < 2)
-                {
-                    return;
-                }
+                    if (!int.TryParse(nextRawPacketId, out int nextPacketId) && nextPacketId != LastPacketId + 1)
+                    {
+                        return;
+                    }
 
-                if (int.TryParse(sessionParts[1].Split('\\').FirstOrDefault(), out int sessid))
-                {
-                    SessionId = sessid;
-                }
-
-                return;
-            }
-
-            string packetConcatenated = WorldPacketExtension.ClientToServer(data, SessionId);
-
-            foreach (string pd in packetConcatenated.Split(new[] { (char)0xFF }, StringSplitOptions.RemoveEmptyEntries))
-            {
-                string packetstring = pd.Replace('^', ' ');
-                string[] packetsplit = packetstring.Split(' ');
-                string nextRawPacketId = packetsplit[0];
-
-                if (!int.TryParse(nextRawPacketId, out int nextPacketId) && nextPacketId != LastPacketId + 1)
-                {
-                    return;
-                }
-
-                if (nextPacketId == 0)
-                {
-                    if (LastPacketId == ushort.MaxValue)
+                    if (nextPacketId == 0)
+                    {
+                        if (LastPacketId == ushort.MaxValue)
+                        {
+                            LastPacketId = nextPacketId;
+                        }
+                    }
+                    else
                     {
                         LastPacketId = nextPacketId;
                     }
-                }
-                else
-                {
-                    LastPacketId = nextPacketId;
-                }
 
-                if (packetsplit.Length > 1)
-                {
-                    if (packetsplit[1].Length >= 1 && (packetsplit[1][0] == '/' || packetsplit[1][0] == ':' || packetsplit[1][0] == ';'))
+                    if (packetsplit.Length > 1)
                     {
-                        packetsplit[1] = packetsplit[1][0].ToString();
-                        packetstring = pd.Insert(pd.IndexOf(' ') + 2, " ");
-                    }
+                        if (packetsplit[1].Length >= 1 && (packetsplit[1][0] == '/' || packetsplit[1][0] == ':' || packetsplit[1][0] == ';'))
+                        {
+                            packetsplit[1] = packetsplit[1][0].ToString();
+                            packetstring = pd.Insert(pd.IndexOf(' ') + 2, " ");
+                        }
 
-                    if (packetsplit[1] == "0") // Useless packet its just sended to server to tell TcpCLient is still active
-                    {
-                        continue;
-                    }
+                        if (packetsplit[1] == "0") // Useless packet its just sended to server to tell TcpCLient is still active
+                        {
+                            continue;
+                        }
 
-                    Console.WriteLine("[WORLD][SEND] {0}", packetstring);
+                        Console.WriteLine("[WORLD][SEND] {0}", packetstring);
+                    }
                 }
             }
+            catch { }
         }
     }
 }
